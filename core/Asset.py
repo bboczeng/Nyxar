6a19b98620ef3e049314d603783df2c9acd17111
bboczeng <bboczeng@gmail.com>
2018-01-28 00:21:56 -0800

    create csv reader for asset

--- core/Asset.py
+++ core/Asset.py
@@ -7,6 +7,8 @@ This file implements the basic I/O operations for an asset
 """
 
 import os
+import csv
+
 from enum import Enum
 
 class AssetFields(Enum):
@@ -17,22 +19,18 @@ class AssetFields(Enum):
     Volume = 4
 
 
+"""
+The base class for asset
+"""
+
 class AssetBase(object):
 
-    field_names = {
-        AssetFields.High: "high",
-        AssetFields.Low: "low",
-        AssetFields.Open: "open",
-        AssetFields.Close: "close",
-        AssetFields.Volume: "volume",
-    }
-
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.name = name
+        self.data = {}
 
     def read_from_csv(self, file_path):
-        assert os.path.exists(file_path), "csv path : " + file_path + " does not exist."
-        # implement the reading algorithm
+        pass
 
     def read_from_url(self, url):
         pass
@@ -41,25 +39,62 @@ class AssetBase(object):
         pass
 
     def __get_value(self, index, field):
-        ## implement in subclass
         pass
 
     @property
     def price_high(self, index):
-        return self.__get_value(index, self.__class__.field_names[AssetFields.High])
+        return self.__get_value(index, AssetFields.High)
 
     @property
     def price_low(self, index):
-        return self.__get_value(index, self.__class__.field_names[AssetFields.Low])
+        return self.__get_value(index, AssetFields.Low)
 
     @property
     def price_open(self, index):
-        return self.__get_value(index, self.__class__.field_names[AssetFields.Open])
+        return self.__get_value(index, AssetFields.Open)
 
     @property
     def price_close(self, index):
-        return self.__get_value(index, self.__class__.field_names[AssetFields.Close])
+        return self.__get_value(index, AssetFields.Close)
 
     @property
     def price_volume(self, index):
-        return self.__get_value(index, self.__class__.field_names[AssetFields.Volume])
+        return self.__get_value(index, AssetFields.Volume)
+
+
+"""
+Asset class for 
+"""
+
+class AssetCSV(AssetBase):
+    def __init__(self, name, file_path):
+        super(AssetCSV, self).__init__(name)
+        self.file_path = file_path
+        # read data
+        self.data = self.read_from_csv(file_path)
+
+    def read_from_csv(self, file_path):
+        assert os.path.exists(file_path), "csv path : " + file_path + " does not exist."
+        # implement the reading algorithm
+        with open(file_path) as input_file:
+            reader = csv.reader(input_file, delimiter=',')
+            count = 0
+            # data format: time_stamp and OHLC, open, high, low, close,
+            for each in reader:
+                assert len(each) == 5, "format error for CSV dataset, it has to be timestamp + OHLC"
+                if count == 0:
+                    count += 1
+                    continue
+                time_stamp = each[0]
+                price_data = {
+                    AssetFields.Open: float(each[1]),
+                    AssetFields.High: float(each[2]),
+                    AssetFields.Low: float(each[3]),
+                    AssetFields.Close: float(each[4])}
+                self.data[time_stamp] = price_data
+
+    def __get_value(self, index, field):
+        assert isinstance(field, AssetFields), "invalid field id: " + field
+        if index not in self.data:
+            raise KeyError("timestamp index: " + index + "does not exist for asset " + self.name)
+        return self.data[index][field]
