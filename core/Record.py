2d43d2d4a247412824bb2f4ef4db62ac3257c106
Huitao Shen <huitao@mit.edu>
2018-01-31 17:27:31 -0500

    Record Instructions

--- core/Record.py
+++ core/Record.py
@@ -1,39 +1,83 @@
-# use a dict/list to maintain the position for every tradable assets
-# use two lists to store history order and open order
-# suggest create a class for order
-# open order structure:
-# timestamp (when placing it) | pair | order type: makret/limit | a bool for buy/sell | number | filled percentage
-# history order structure
-# timestamp (when filled) | pair | a bool for buy/sell | number
+##############
+# For reference, here is the structure of order returned by ccxt library:
+#
+#{
+#    'id':        '12345-67890:09876/54321', // string
+#    'datetime':  '2017-08-17 12:42:48.000', // ISO8601 datetime with milliseconds
+#    'timestamp':  1502962946216, // Unix timestamp in milliseconds
+#    'status':    'open',         // 'open', 'closed', 'canceled'
+#    'symbol':    'ETH/BTC',      // symbol
+#    'type':      'limit',        // 'market', 'limit'
+#    'side':      'buy',          // 'buy', 'sell'
+#    'price':      0.06917684,    // float price in quote currency
+#    'amount':     1.5,           // ordered amount of base currency
+#    'filled':     1.0,           // filled amount of base currency
+#    'remaining':  0.5,           // remaining amount to fill
+#    'trades':   [ ... ],         // a list of order trades/executions
+#    'fee':      {                // fee info, if available
+#        'currency': 'BTC',       // which currency the fee is (usually quote)
+#        'cost': 0.0009,          // the fee amount in that currency
+#    },
+#    'info':     { ... },         // the original unparsed order structure as is
+#}
+#
+# Regarding our implementation of order class:
+#
+#	1. Any particular reason why timestamp should be string? Just use an integer.
+#	2. I suggest create a dict for all supported currencies and an corresponding ID. 
+#      Instead of saving Order.name, we should save Order.quote_currency and Order.base_currency as ID, 
+#      and save Order.side as 'buy' or 'sell'. This will allow easy searching. 
+#      We can of course provide a method converting these ID to pair name like "ETH/BTC". 
+#   3. Should add an ID for each order, enabling the TradeAlgo to query and cancel that order. 
+#   4. Instead of saving Order.fill_percentage, save Order.filled. Percentage and remaining should be 
+#      calculated through provided method. 
+#   5. The balance of each currency can be a list, accessed by currency ID. 
+#   
+# Regarding our general implementation:
+#   1. We don't need to distinguish open order and order. All orders should have filled amount. 
+#      (See History order in binance)
+#   2. We also need to create a trade class, and store all past trades. See "trades" in the previous structure,
+#      and the following 
+#   3. Here is what happens in every time cycle: 
+#	   Note that the order placed on this timestamp will always be processed at next timestamp.
+#       a. Update the latest market price based on timestamp. 
+#       b. Resolve all open orders in OpenOrder. 
+#          1) For limit order, check if the current price meets the fill condition. 
+#		   2) Set the price to be this timestamps close price, for fulfilled limit order or market order.
+#			  (This part should be tunable. As one may also want to use high price for buy and low price 
+#			   for sell in order to get more realistic results. )
+#		   3) Call predefined slippage model to determine how the orders are filled (volume, fee, etc). 
+#		   4) Create corresponding trades and store in history trades. 
+#		   5) Update Order.filled and append Order.trades. Both OpenOrder and HistoryOrder should be updated 
+#			  automatically, as only the reference is saved. 
+#		   6) Update account balance. 
+#       c. Accepet requests from TradeAlgo, including
+#		   1) Place/cancel/query order (through order ID). 
+#          2) Query account balance.
+#		   3) Query market. (Not sure if we just automatically send the latest market data to TradeAlgo. )
+#          4) Query history trades or orderbook. 
+#		d. Before placing an order, first check if the account balance is enough. 
+#          As long as an order is placed, we create a new Order object, and put its reference in both 
+#          HistoryOrder and OpenOrder. 
+#
+# More on Slippage model:
+#   Exchange should be able to accept (optional) bid/ask. The slippage model should be able to run based
+#   on it. I think slippage is particularly important in cryptocurrency, due to the lack of liquidity and
+#   large bid-ask spread for altcoins. 
+#   
+#   Here is the API for slippage model in Zipline:
+#   ``Your custom model must be a class that inherits from slippage.SlippageModel and implements 
+#     process_order(self, data, order). The process_order method must return a tuple of 
+#     (execution_price, execution_volume), which signifies the price and volume for the transaction that 
+#     your model wants to generate. The transaction is then created for you. ''
+#
+# More features to add:
+#   1. Stop-loss/Stop-limit order. It is important because the order is placed and executed at different time. 
+#   2. More slippage model.
+#
+#
+#############
 
-# do you think it is necessary to give every asset an unique number ID?
-
-# here is what happens when a new timestamp is being processed:
-# exchange check if there are open orders remaining
-#   - If there are, check if orders can be filled or not, then update the position
-# exchange sends the latest quote to the trading algo
-# exchange takes request (place order / cancel order / history order / current position) from the trading algo, 
-# then process it
-
-# not sure about the time ordering here. but i think one should place order at current tick and process it at the
-# next tick? or order is always processed within the same price in the same tick
-
-
-# How to check if orders can be filled? (For OHLCV data)
-# Market order:
-# 1. Set the price to be this tick's close price (should be flexible to tune this. for example, use high for buy or
-#    low to sell)
-# 2. Slippage model. Here we first implement a fixed basispoint model. 0.05% fee for all trading, i.e., additional 
-#    0.05% for buy, less 0.05% for sell. 
-# 3. Check if there are enough quote currency based on the price. If not, (i never use market order. don't know if 
-#    we should reject the order, or fill as much as possible)
-# 4. If the order is only paritially filled, update the filled percentage and leave it in the open order for next cycle
-
-
-# more to come
-# exchange should be able to accept (optional) bid/ask, along with OHLCV. the slippage model should be able to run based
-# on it. i think slippage is particularly important in cryptocurrency, due to the lack of liquidity and large bid-ask 
-# spread for altcoins
 
 from enum import Enum
 import math
