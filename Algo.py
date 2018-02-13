eb8f015d0233a23df5b3e476e3ebf27ea5171cfb
Huitao Shen <huitao@mit.edu>
2018-02-13 01:52:42 -0500

    Revert "Revert "Revert "minor bugs fixed"""
    This reverts commit f20a11f2c9f0ddb1eeb76763fa6c096a02ef4813.

--- Algo.py
+++ Algo.py
@@ -1,28 +1,28 @@
-from backtest.backexchange import BackExchange
+from backtest.BackExchange import BackExchange
+from backtest.Order import OrderSide
 
 
 class TradingAlgo(object):
     def __init__(self, exchange: BackExchange):
-        self.exchange = exchange
+        self.exchange_api = exchange
 
     def display_balance(self):
-        balance = self.exchange.fetch_balance()
+        balance = self.exchange_api.get_balance()
         for asset in balance:
             if balance[asset]['total'] > 0:
                 print("{:<4} Total:{:>12}  In order:{:>12}  Remaining:{:>12}".format(
-                    asset, round(balance[asset]['total'], 8), round(balance[asset]['in order'], 8),
-                    round(balance[asset]['available'], 8)))
-
-    def initialize(self):
-        self.last_price = 0
+                    asset, round(balance[asset]['total'], 8), round(balance[asset]['in_order'], 8),
+                    round(balance[asset]['total'] - balance[asset]['in_order'], 8)))
 
     def execute(self):
-        print(self.exchange.fetch_ticker('XRP/ETH'))
-        current_price = self.exchange.fetch_ticker('XRP/ETH')['open']
-        balance = self.exchange.fetch_balance()
-        if current_price < self.last_price:
-            id = self.exchange.create_limit_buy_order(symbol='XRP/ETH', amount=10, price=current_price)
-        elif balance['XRP']['available'] >= 10.0:
-            id = self.exchange.create_limit_sell_order(symbol='XRP/ETH', amount=10, price=current_price)
-        self.last_price = current_price
+        print("Timestamp: ", self.exchange_api.current_timestamp)
+        data = self.exchange_api.get_price('XRP/ETH')
+        if 0.00107 < data['open'] < 0.001078:
+            self.exchange_api.place_limit_order('XRP', 'ETH', 0.001078, 1000, OrderSide.Buy)
+        else:
+            available = self.exchange_api.get_balance()['XRP']['total'] - \
+                        self.exchange_api.get_balance()['XRP']['in_order']
+            if available > 0:
+                print("Available", available)
+                self.exchange_api.place_limit_order('XRP', 'ETH', 0.001078, available, OrderSide.Sell)
         self.display_balance()
