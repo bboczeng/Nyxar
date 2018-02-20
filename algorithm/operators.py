08c159ea4c472b522f67200cde3de89732431ef1
bboczeng <bboczeng@gmail.com>
2018-02-20 00:11:43 -0800

    add __get_feed and RSI, SMMA
    refactor to have __get_feed to enable custom input to indicators
    by this, we implemented RSI via SMMA

--- algorithm/operators.py
+++ algorithm/operators.py
@@ -29,6 +29,17 @@ class OperatorsBase(object):
     def get(self):
         pass
 
+    """
+    This is something that should only be used internally, 
+    but support is added here nevertheless to facilitate
+    highly customized indicator of indicators, like RSI.
+    in get_feed, every time stamp value is sent in directly 
+    rather than read from the exchange
+    """
+    def __get_feed(self, value):
+        pass
+
+
 
 class EMA(OperatorsBase):
     def __init__(self, exchange: BackExchange, ticker_name: str, window_size : int, field: QuoteFields):
@@ -42,18 +53,21 @@ class EMA(OperatorsBase):
         self.operator_name = "EMA(" + str(window_size) + ")" + " of " + ticker_name
 
     def get(self):
+        current_price = self.exchange.fetch_ticker(self.ticker_name)[self.field]
+        return self.__get_feed(current_price)
+
+    def __get_feed(self, value):
         if self.last_timestamp == self.exchange.current_timestamp:
             print("You attempt to calculate {} twice at ts={}".format(self.operator_name, self.last_timestamp))
             print("Please save it to a local variable and reuse it elsewhere, now using calculated value.")
             return self.ema
-        current_price = self.exchange.fetch_ticker(self.ticker_name)[self.field]
-        self.price_queue.append(current_price)
+        self.price_queue.append(value)
         if len(self.price_queue) < self.window_size:
             return self.ema
         elif len(self.price_queue) == self.window_size:
             self.ema = sum(self.price_queue) / self.window_size
         else:
-            self.ema += (current_price - self.price_queue.popleft()) * self.multiplier
+            self.ema += (value - self.price_queue.popleft()) * self.multiplier
         self.last_timestamp = self.exchange.current_timestamp
         return self.ema
 
@@ -70,20 +84,24 @@ class SMA(OperatorsBase):
         self.operator_name = "SMA(" + str(window_size) + ")" + " of " + ticker_name
 
     def get(self):
+        current_price = self.exchange.fetch_ticker(self.ticker_name)[self.field]
+        return self.__get_feed(current_price)
+
+    def __get_feed(self, value):
         if self.last_timestamp == self.exchange.current_timestamp:
             print("You attempt to calculate {} twice at ts={}".format(self.operator_name, self.last_timestamp))
             print("Please save it to a local variable and reuse it elsewhere, now using calculated value.")
             return self.sma
-        current_price = self.exchange.fetch_ticker(self.ticker_name)[self.field]
-        self.price_queue.append(current_price)
+        self.price_queue.append(value)
         if len(self.price_queue) < self.window_size:
             return self.sma
         elif len(self.price_queue) == self.window_size:
             self.sma = sum(self.price_queue) / self.window_size
         else:
-            self.sma += (current_price - self.price_queue.popleft()) / self.window_size
+            self.sma += (value - self.price_queue.popleft()) / self.window_size
         self.last_timestamp = self.exchange.current_timestamp
         return self.sma
+
 
 
 """
@@ -106,10 +124,12 @@ class MACD(OperatorsBase):
         ema_12 = self.ema_12.get()
         ema_26 = self.ema_26.get()
         if ema_12 is None or ema_26 is None:
-            return None
+            self.macd = None
         else:
             self.macd = ema_12 - ema_26
-            return self.macd
+        self.last_timestamp = self.exchange.current_timestamp
+        return self.macd
+
 
 
 """
@@ -124,25 +144,103 @@ class StochasticOscillator(OperatorsBase):
         self.low_14 = None
         self.high_14 = None
         self.price_queue = deque(maxlen=14)
-        self.stochastic_oscillator = None
+        self.percent_k = None
         self.past_oscillator = deque(maxlen=3)
+        self.percent_d = None
         self.operator_name = "StochasticOscillator" + " of " + ticker_name
 
     def get(self):
+        current_close = self.exchange.fetch_ticker(self.ticker_name)[QuoteFields.Close]
+        return self.__get_feed(current_close)
+
+    def __get_feed(self, value):
         if self.last_timestamp == self.exchange.current_timestamp:
             print("You attempt to calculate {} twice at ts={}".format(self.operator_name, self.last_timestamp))
             print("Please save it to a local variable and reuse it elsewhere, now using calculated value.")
-            return self.stochastic_oscillator
+            return self.percent_k, self.percent_d
 
-        current_close = self.exchange.fetch_ticker(self.ticker_name)[QuoteFields.Close]
         if len(price_queue) < 14:
-            self.price_queue.append(current_close)
-            return None
-        self.low_14 = min(price_queue)
-        self.high_14 = max(price_queue)
-        self.price_queue.append(current_close)
-        self.stochastic_oscillator = round((current_close - self.low_14) / (self.high_14 - self.low_14) * 100, 2)
-        self.past_oscillator.append(self.stochastic_oscillator)
-        if len(self.past_oscillator) < 3:
-            return self.stochastic_oscillator, None
-        return self.stochastic_oscillator, round(sum(self.past_oscillator) / 3, 2)
+            self.price_queue.append(value)
+        else:
+            self.low_14 = min(price_queue)
+            self.high_14 = max(price_queue)
+            self.price_queue.append(value)
+            self.stochastic_oscillator = round((value - self.low_14) / (self.high_14 - self.low_14) * 100, 2)
+            self.past_oscillator.append(self.stochastic_oscillator)
+            self.last_timestamp = self.exchange.current_timestamp
+            if len(self.past_oscillator) == 3:
+                self.percent_d = round(sum(self.past_oscillator) / 3, 2)
+        self.last_timestamp = self.exchange.current_timestamp
+        return self.percent_k, self.percent_d
+
+
+"""
+SMMA, the smoothed moving average
+This indicator is used to facilitate standard RSI calculations.
+"""
+class SMMA(OperatorsBase):
+    def __init__(self, exchange: BackExchange, ticker_name: str, window_size : int, field: QuoteFields):
+        super(SMA, self).__init__(exchange)
+        self.ticker_name = ticker_name
+        self.window_size = window_size
+        self.field = field
+        self.smma = None
+        self.operator_name = "SMMA(" + str(window_size) + ")" + " of " + ticker_name
+
+    def get(self):
+        current_price = self.exchange.fetch_ticker(self.ticker_name)[self.field]
+        return self.__get_feed(current_price)
+
+    def __get_feed(self, value):
+        if self.last_timestamp == self.exchange.current_timestamp:
+            print("You attempt to calculate {} twice at ts={}".format(self.operator_name, self.last_timestamp))
+            print("Please save it to a local variable and reuse it elsewhere, now using calculated value.")
+            return self.smma
+        if self.smma is None:
+            self.smma = value
+        else:
+            self.smma = (self.smma*(self.window_size - 1) + value) / self.window_size
+        self.last_timestamp = self.exchange.current_timestamp
+        return self.smma
+
+    """
+    expose __get_feed to external use by choice
+    """
+    def get_feed_extern(self, value):
+        return self.__get_feed(value)
+
+
+"""
+RSI Index
+it returns the RSI index calculated with smoothed SMA for ups and downs.
+it is also an indicator of indicators
+"""
+class RSI(OperatorsBase):
+    def __init__(self, exchange: BackExchange, ticker_name: str, window_size: int = 14):
+        super(SMA, self).__init__(exchange)
+        self.ticker_name = ticker_name
+        self.window_size = window_size
+        self.smma_up = SMMA(BackExchange, ticker_name, window_size, QuoteFields.Close)
+        self.smma_down = SMMA(BackExchange, ticker_name, window_size, QuoteFields.Close)
+        self.rsi = None
+        self.close_prev = None
+        self.operator_name = "RSI(" + self.window_size + ")" + " of " + ticker_name
+
+    def get(self):
+        current_close = self.exchange.fetch_ticker(self.ticker_name)[QuoteFields.Close]
+        return self.__get_feed(current_close)
+
+    def __get_feed(self, value):
+        if self.last_timestamp == self.exchange.current_timestamp:
+            print("You attempt to calculate {} twice at ts={}".format(self.operator_name, self.last_timestamp))
+            print("Please save it to a local variable and reuse it elsewhere, now using calculated value.")
+            return self.rsi
+        if self.close_prev is None:
+            return self.rsi
+        up_price = max(0, value - self.close_prev)
+        down_price = max(0, self.close_prev - value)
+        smma_u = self.smma_up.get_feed_extern(up_price)
+        smma_d = self.smma_down.get_feed_extern(down_price)
+        self.rsi = 100 - 100 / (1 + smma_u / smma_d)
+        self.last_timestamp = self.exchange.current_timestamp
+        return self.rsi
